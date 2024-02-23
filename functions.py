65d6a04b16caa9987bdd37614f232a87f0b5e8b4
Ashish Sapkota <aacsspkt3@gmail.com>
2024-02-23 16:15:48 +0545

    function and loops

---
+++ functions.py
@@ -0,0 +1,22 @@
+
+def function_name(args, args1):
+    print("task")
+
+def calculate_area(base, height):
+    return (1 / 2) * base * height
+
+b = 1
+h = 3
+area = calculate_area(b, h)
+print("area:", area)
+
+def get_list():
+    return [1,2,3]
+
+list = get_list()
+
+for item in list:
+    print(item)
+
+
+# write two function that calculate volume of cube and cuboid
---
+++ loop.py
@@ -0,0 +1,15 @@
+list = ['a', 'b', 'c', 'd']
+
+
+# for item in list:
+#     print(item)
+
+# index = 0
+# while index < len(list):
+#     print(list[index]) 
+#     index+=1
+
+
+for i in range(10):
+    print(i)
+
