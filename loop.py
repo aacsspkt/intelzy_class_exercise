048374507d8a2a1173b4fcdf0d375172285aaf59
Ashish Sapkota <aacsspkt3@gmail.com>
2024-02-25 16:22:27 +0545

    for loop and while loop

--- loop.py
+++ loop.py
@@ -1,15 +1,19 @@
 list = ['a', 'b', 'c', 'd']
 
+iter = iter(list)
 
-# for item in list:
-#     print(item)
+def lazy_returner():
+    yield 'apple'
+    yield 'banana'
+
+def do_something(item):
+    print(item)
 
 # index = 0
 # while index < len(list):
 #     print(list[index]) 
 #     index+=1
 
-
-for i in range(10):
-    print(i)
-
+len = 10
+for num in range(len):
+    print("*" * (2 * num + 1 ))
