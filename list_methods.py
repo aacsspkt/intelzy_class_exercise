b49d42540d3aaa5e0721aa31462e85e283acd8fa
Ashish Sapkota <aacsspkt3@gmail.com>
2024-02-27 13:25:06 +0545

    list methods

--- list_methods.py
+++ list_methods.py
@@ -2,34 +2,39 @@ spr = ["scissor", "paper", "rock", "scissor"]
 
 print("spr:", spr)
 
-spr.append("mud")
-print("spr:", spr)
+# spr.append("mud")
+# print("spr:", spr)
 
-item = spr.pop()
-print("spr:", spr)
+# item = spr.pop()
+# print("popped item:", item)
+# print("spr:", spr)
 
-print("scissor count:", spr.count("scissor"))
+# print("scissor count:", spr.count("paper"))
 
-fruits = ["apple", "banana", "orange"]
-spr.extend(fruits)
+# fruits = ["apple", "banana", "orange"]
+# spr.extend(fruits)
+# # new = spr + fruits
+# print("spr", spr)
 
-print("spr", spr)
+# spr.remove("apple")
+# print("spr", spr)
 
-spr.remove("apple")
-print("spr", spr)
+# another_spr = spr.copy()
+# print("another spr:", another_spr)
 
-another_spr = spr.copy()
-print("another spr:", another_spr)
+# i = spr.index("paper")
+# print("paper is at index:", i)
 
-index = spr.index("paper")
-print("paper is at index:", index)
-
-spr.reverse()
-print("spr", spr)
+# spr.reverse()
+# print("spr", spr)
 
 # sort ascending
 spr.sort() 
 print("spr sorted", spr)
+
+num_list = [10, 9, 6, 20, 7]
+num_list.sort()
+print("num sorted", num_list)
 
 # sort descending
 spr.sort(reverse=True)
@@ -37,3 +42,6 @@ print("spr sorted", spr)
 
 spr.clear()
 print("spr", spr)
+
+second = spr[1]
+print()
