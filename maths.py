5a95742a1f3c1860a52b7d3597cb5f6de85f0c92
Ashish Sapkota <aacsspkt3@gmail.com>
2024-02-28 16:44:18 +0545

    builtin math function and math module

---
+++ maths.py
@@ -0,0 +1,21 @@
+import math
+
+a = 100
+b = 200
+
+m = min(a, b)
+print("min:", m)
+
+mx = max(a, b)
+print("max:", mx)
+
+sqrt = math.sqrt(64)
+
+print("sqrt:", sqrt)
+
+num = 109.425
+floored = math.floor(num)
+print("floored:", floored)
+
+ceil = math.ceil(num)
+print("ceil:", ceil)
